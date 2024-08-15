e94c26707432eb8f77f21c2a97c4056f19aaf441
Marie <mariegunduz@gmail.com>
2024-08-15 22:59:34 +0300

    adding the sign in button to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,6 +17,11 @@ const Navbar = () => {
               </li>
             ))}
           </ul>
+          <div className="hidden lg:flex justify-center space-x-12 items-center">
+            <a href="#" className="py-2 px-3 border rounded-md">
+              sign In
+            </a>
+          </div>
         </div>
       </div>
     </nav>
