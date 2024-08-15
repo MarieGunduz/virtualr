1a8db016a83db66e17adfabce9360a438b23f792
Marie <mariegunduz@gmail.com>
2024-08-15 23:13:11 +0300

    changed position of button on Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,5 @@
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
 import logo from "../assets/logo.png";
 import { navItems } from "../constants";
 
@@ -5,7 +7,7 @@ const Navbar = () => {
   return (
     <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
       <div className="container px-4 mx-auto realtive text-sm">
-        <div className="flex justify-center items-center">
+        <div className="flex justify-between items-center">
           <div className="flex items-center flex-shrink-0">
             <img className="h-10 w-10 mr-2" src={logo} alt="logo" />
             <span className="text-xl tracking-tight">Virturalr</span>
